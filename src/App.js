90c495848fbabaa850e84f6ab7e05e8b265cdca6
sergiop45 <sergiopsantos33@gmail.com>
2022-06-16 14:20:04 -0300

    primeiraparte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch} from 'react-icons/fi'
+import './App.css'
+import { useState } from 'react'
+import api from './services/api'
+
+
+
+
 
 function App() {
+
+  const [input, setInput] =  useState("")
+  const [cep, setCep] = useState({})
+
+  async function localize() {
+    if(input === "") {
+      alert("digite um cep");
+      return;
+    }
+
+    try {
+      const response = await api.get(input + "/json");
+      setCep(response.data)
+      setInput("")
+    } catch {
+      alert("erro ao buscar CEP!")
+      setInput("")
+    }
+
+    
+
+   
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+     <h1>LocalizeComCep</h1>
+      <div className="boxInput">
+        <input type="text" placeholder="Digite aqui o Cep..."
+        value={input} onChange={(event) => setInput(event.target.value) }
         >
-          Learn React
-        </a>
-      </header>
+          
+        </input>
+        <button className="btnLocalize" onClick={localize}>
+          <FiSearch size={25} color="gold"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2 className="title">CEP: {cep.cep}</h2>
+
+        <span className="info">Rua: {cep.logradouro}</span>
+        <span className="info">Complemento: {cep.complemento}</span>
+        <span className="info">Bairro: {cep.bairro}</span>
+        <span className="info">Localidade: {cep.localidade} - {cep.uf}</span>
+      </main>
     </div>
   );
 }
